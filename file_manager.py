a1bcc718fe739790555f91eedc58f26d0cc55186
Ben8282 <benchorev@gmail.com>
2026-08-16 23:29:01 +0700

    Delete file_manager.py

--- file_manager.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-import shutil
-def move_file_to_folder(file_path, folder_name):
-    # Create the folder if it doesn't exist
-    pathlib.Path(folder_name).mkdir(parents=True, exist_ok=True)
-    
-    # Move the file to the folder
-    shutil.move(file_path, pathlib.Path(folder_name) / pathlib.Path(file_path).name)
